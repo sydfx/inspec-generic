bbd96278258eaf7ac1d951e33429b0842c94660a
David Guevremont <david.guevremont@videri.com>
2018-04-14 02:13:21 -0400

    adding ref, to develop the tests.

--- aws/controls/monitoring.rb
+++ aws/controls/monitoring.rb
@@ -2,6 +2,12 @@
 # copyright: 2018, The Authors
 
 title 'Dynamic test - AWS Services; Monitoring tools'
+# ref
+# https://www.inspec.io/docs/reference/resources/aws_cloudtrail_trail
+# https://www.inspec.io/docs/reference/resources/aws_cloudtrail_trails
+# https://www.inspec.io/docs/reference/resources/aws_cloudwatch_alarm
+# https://www.inspec.io/docs/reference/resources/aws_cloudwatch_log_metric_filter
+# https://www.inspec.io/docs/reference/resources/aws_config_recorder
 
 # Attributes here.
 content = inspec.profile.file("monitoring-config.JSON")
