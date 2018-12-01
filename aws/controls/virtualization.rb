59cb6374859334d2e950eca7425323cb5beac693
David Guevremont <david.guevremont@videri.com>
2018-11-30 21:57:11 -0500

    Adding virtualization file to the aws-resource.

--- aws/controls/virtualization.rb
+++ aws/controls/virtualization.rb
@@ -1,3 +1,13 @@
+## Resource to cover.
+
+# aws_ebs_volume
+# aws_ebs_volumes
+# aws_ec2_instance
+# aws_ec2_instances
+# aws_eks_cluster
+# aws_elb
+# aws_elbs
+
 # encoding: utf-8
 # copyright: 2018, D.
 
@@ -42,55 +52,3 @@ describe aws_cloudtrail_trail(trail_name) do
   its('home_region') { should include (region) }
  end
 end
-
-control 'monitoring-02' do
-  impact 1.0
-  title 'Cloudtrail - Delivery time is recent'
-  desc 'This test ensure the latest delivery time was recent - value 0 day late'
-  describe aws_cloudtrail_trail(trail_name) do
-    its('delivered_logs_days_ago') { should eq 0 }
-  end
-end
-
-control 'monitoring-03' do
-  impact 1.0
-  title 'Cloudtrail - Validation over the trails list'
-  desc 'This is to validate specific values within the trails list'
-  describe aws_cloudtrail_trails do
-    its('names') { should include(trails_name) }
-    its('trail_arns') { should include(tr_arns) }
- end
-end
-
-control 'monitoring-04' do
-  impact 1.0
-  title 'Cloudtrail - Maximum Cloudtrail Trails allowed'
-  desc 'Simply validate a maximum number of Cloudtrail Trails within an AWS account.'
-  describe aws_cloudtrail_trails do
-    its('entries.count') { should be <= (trails_count)}
- end
-end
-
-### I must come back to CLOUDWATCH properties..
-### Also add flow logs in this file
-
-# # List of objects variables for AWS Cloudwatch Alarm
-# webserver_id = attribute('metric', description: '')
-# webserver_id = attribute('metric.namespace', description: '')
-#
-# # List of objects variables for AWS Cloudwatch log metric filter
-# Filter attributes:
-#    filter_name, log_group_name, pattern
-# Properties:
-#    filter_name, log_group_name,metric_name, metric_namespace, pattern
-# webserver_id = attribute('filter.name', description: '')
-# ec2_ami_id = attribute('log.group.name', description: '')
-# ec2_type = attribute('pattern.id', description: '')
-#
-# # List of objects variables for AWS Config Recorder
-# webserver_id = attribute('ec2_instance.webserver', description: '')
-# ec2_ami_id = attribute('ec2_instance.webserver.ami', description: '')
-# ec2_type = attribute('ec2_instance.webserver.instance_type', description: '')
-# vpc_identifier = attribute('vpc.id', description: '')
-
-# Controls here.
