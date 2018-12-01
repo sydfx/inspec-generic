d690e210dca307eedda262ec454d1b2f113495b1
David Guevremont <david.guevremont@videri.com>
2018-11-30 23:57:27 -0500

    preparation of the file for the virtualization test

--- aws/controls/virtualization.rb
+++ aws/controls/virtualization.rb
@@ -11,44 +11,48 @@
 # encoding: utf-8
 # copyright: 2018, D.
 
-title 'Dynamic test - AWS Services; Monitoring tools'
+title 'Dynamic test - AWS Services; Virtualization tools'
 # ref
-# https://www.inspec.io/docs/reference/resources/aws_cloudtrail_trail
-# https://www.inspec.io/docs/reference/resources/aws_cloudtrail_trails
-# https://www.inspec.io/docs/reference/resources/aws_cloudwatch_alarm
-# https://www.inspec.io/docs/reference/resources/aws_cloudwatch_log_metric_filter
-# https://www.inspec.io/docs/reference/resources/aws_config_recorder
+# https://www.inspec.io/docs/reference/resources/aws_ebs_volume/
+# https://www.inspec.io/docs/reference/resources/aws_ebs_volumes/
+# https://www.inspec.io/docs/reference/resources/aws_ec2_instance/
+# https://www.inspec.io/docs/reference/resources/aws_ec2_instances/
+# https://www.inspec.io/docs/reference/resources/aws_eks_cluster/
+# https://www.inspec.io/docs/reference/resources/aws_elb/
+# https://www.inspec.io/docs/reference/resources/aws_elbs/
 
 # Attributes here.
-# List of objects variables for AWS Cloudtrail  / Cloudtrail trails
-trail_name = attribute('trail.name', description: '')
-s3 = attribute('s3bucket.name', description: '')
-tr_arn_id = attribute('trail_arn.id', description: '')
-cw_logs_rarn = attribute('cloudwatch_logs_role.arn', description: '')
-cw_logs_garn = attribute('cloudwatch_logs_log_group.arn', description: '')
-kms_key = attribute('kms_key.id', description: '')
-region = attribute('home_region.id', description: '')
-# Attributes for the Trails
-trails_count = attribute('cloudtrail.entries', description: '')
-trails_name = attribute('cloudtrail.name.list', description: '')
-tr_arns = attribute('cloudtrail.arn.list', description: '')
+# List of objects variables for EBS Volume  / EBS Volumes
+volume_id = attribute('volume.id', description: '')
+az = attribute('availability.zone', description: '')
+ebs_size = attribute('size', description: '')
+iops = attribute('iops', description: '')
+snapshot_id = attribute('snapshot.id', description: '')
+ebs_state = attribute('state', description: '')
+type = attribute('volume.type', description: '')
+kms_key = attribute('kms.key.id', description: '')
+# Attributes for the Volumes
+# entries = attribute('entries.count', description: '')
+# volume_ids = attribute('volume.ids', description: '')
+
 
 
 # Controls here
 # For security measures the test enforced encryption, log validation and multi-region on the log file.
 control 'monitoring-01' do
   impact 1.0
-  title 'Cloudtrail validation'
-  desc 'This test covers only 1 trail and should be copied for each trail that need to be tested'
-describe aws_cloudtrail_trail(trail_name) do
-  it { should be_log_file_validation_enabled }
+  title 'EBS Validation'
+  desc 'This test covers only 1 EBS volume and should be copied for each volume that need to be tested'
+describe aws_ebs_volume(volume_id) do
+  it { should exist }
   it { should be_encrypted }
-  it { should be_multi_region_trail }
-  its('s3_bucket_name') { should eq (s3) }
-  its('trail_arn') { should cmp (tr_arn_id) }
-  its('cloud_watch_logs_role_arn') { should include (cw_logs_rarn) }
-  its('cloud_watch_logs_log_group_arn') { should include (cw_logs_garn) }
+  its('availability_zone') { should eq (az) }
+  its('size') { should cmp (ebs_size) }
+  its('iops') { should cmp (iops) }
+  its('snapshot_id') { should be (snapshot_id) }
+  its('state') { should eq (ebs_state) }
+  its('volume_type') { should eq (type) }
   its('kms_key_id') { should include (kms_key) }
-  its('home_region') { should include (region) }
+
  end
 end
